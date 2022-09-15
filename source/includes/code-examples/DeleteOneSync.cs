59d118392afd1c781e97b4752dd51cc1f76cdb9c
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2022-09-15 16:29:47 -0400

    DOCSP-24536: deleteOne usage ex

---
+++ source/includes/code-examples/DeleteOneSync.cs
@@ -0,0 +1,70 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using static System.Console;
+
+namespace CSharpExamples.UsageExamples;
+
+public class DeleteOneSync
+{
+    private static IMongoCollection<Restaurant> _restaurantsCollection;
+    private static string _mongoConnectionString = "<Your MongoDB URI>";
+
+    public static void Main(string[] args)
+    {
+        Setup();
+
+        // Delete a document using builders
+        WriteLine("Deleting a document with builders...:");
+        var result = DeleteARestaurantBuilderSync();
+
+        WriteLine($"Deleted documents: {result.DeletedCount}");
+        // Extra space for console readability 
+        WriteLine();
+        
+    }
+
+    private static DeleteResult DeleteARestaurantBuilderSync()
+    {
+        // start-delete-one-builders-sync
+        var filter = Builders<Restaurant>.Filter
+            .Eq("cuisine", "Continental");
+
+        var result = _restaurantsCollection.DeleteOne(filter);
+        return result;
+        // end-delete-one-builders-sync
+    }
+
+    private static void Setup()
+    {
+        // This allows automapping of the camelCase database fields to our models. 
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // Establish the connection to MongoDB and get the restaurants database
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var restaurantsDatabase = mongoClient.GetDatabase("sample_restaurants");
+        _restaurantsCollection = restaurantsDatabase.GetCollection<Restaurant>("restaurants");
+    }
+}
+
+// start-model
+public class Restaurant
+{
+    public ObjectId Id { get; set; }
+
+    public string Name { get; set; }
+
+    [BsonElement("restaurant_id")]
+    public string RestaurantId { get; set; }
+
+    public string Cuisine { get; set; }
+
+    public object Address { get; set; }
+
+    public string Borough { get; set; }
+
+    public List<object> Grades { get; set; }
+}
+// end-model

153ba8a27e916f6729d0b7871a816212dfc794ae
Mike Woofter <mike.woofter@mongodb.com>
2022-09-15 10:59:38 -0500

    shorten class

--- source/includes/code-examples/update-many/Restaurant.cs
+++ source/includes/code-examples/update-many/Restaurant.cs
@@ -7,18 +7,7 @@ public class Restaurant
 {
     public ObjectId Id { get; set; }
 
-    public string Name { get; set; }
-
-    [BsonElement("restaurant_id")]
-    public string RestaurantId { get; set; }
-
     [BsonElement("cuisine")]
     public string Cuisine { get; set; }
-
-    public object Address { get; set; }
-
-    public string Borough { get; set; }
-
-    public List<object> Grades { get; set; }
 }
 // end-model

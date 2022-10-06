d19d227e78c9f5c601406d0d19c45161cbaf1f47
Mike Woofter <mike.woofter@mongodb.com>
2022-10-06 08:02:48 -0500

    changes

--- source/includes/fundamentals/code-examples/connection/LocalConnectionConfig.cs
+++ source/includes/fundamentals/code-examples/connection/LocalConnectionConfig.cs
@@ -1,7 +1,7 @@
 using MongoDB.Driver;
 
 // Connection URI
-const string connectionUri = "mongodb://localhost:27017";
+const string connectionUri = "mongodb+srv://sample.host:27017/?connectTimeoutMS=60000&tls=true";
 
 // Create a new client and connect to the server
 var client = new MongoClient(connectionUri);

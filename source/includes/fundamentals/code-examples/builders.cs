98e1a457a636276dcd8ad61bddf19007a44d0187
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2022-10-10 11:49:43 -0400

    first draft with all sections

--- source/includes/fundamentals/code-examples/builders.cs
+++ source/includes/fundamentals/code-examples/builders.cs
@@ -13,14 +13,18 @@ public class Builders
     {
         Setup();
         
-        var builder = Builders<Flower>.Filter;
-        var filter = builder.AnyEq(flower => flower.Season, "winter");
+        // Sample builders definitions
+        var filter = Builders<Flower>.Filter.AnyEq(flower => flower.Season, "spring");
+        var projection = Builders<Flower>.Projection.Include("Name").Include("Price").Exclude("Id");
+        var sort = Builders<Flower>.Sort.Ascending("Price").Descending("Category");
 
         WriteLine("Finding documents...");
-        var result = _flowerCollection.Find(filter).First();
+        var result = _flowerCollection.Find(filter).Sort(sort).Project(projection).toList();
 
         WriteLine(result.ToJson());
-        // WriteLine($"Result acknowledged? {result.IsAcknowledged}");
+
+        var update = Builders<Flower>.Update.Mul("Price", 0.9);
+        var result2 = _flowerCollection.UpdateOne(filter, update);
     }
     private static void Setup()
     {
@@ -39,5 +43,6 @@ public class Flower
     public string Category { get; set; }
     public double Price { get; set; }
     public List<string> Season { get; set; }
+    public double Stock { get; set; }
 }
 // end-model

1f9bb03e2181b6f2de11b659808cd3f7ca40f43f
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2022-10-12 15:33:59 -0400

    DOCSP-24551: work with POCOs

---
+++ source/includes/fundamentals/code-examples/poco.cs
@@ -0,0 +1,48 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TestRun.Fundamentals;
+
+public class Builders
+{
+    private static IMongoCollection<Clothing> _myColl;
+    private static string _mongoConnectionString = "<Your MongoDB URI>";
+    
+    public static void Main(string[] args)
+    {
+        Setup();
+        
+        // start-insert
+        var doc = new Clothing() 
+        { 
+            name = "Denim Jacket", 
+            instock = false, 
+            price = 32.99m, 
+            color = new List<string>() {"dark wash", "light wash"}
+        };
+        
+        _myColl.InsertOne(doc);
+        // end-insert
+    }
+    private static void Setup()
+    {
+        // Establish the connection to MongoDB and get the restaurants database
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var myDatabase = mongoClient.GetDatabase("sample_db");
+        _myColl = myDatabase.GetCollection<Clothing>("sample_coll");
+    }
+}
+
+// start-model
+public class Clothing
+{
+    public ObjectId _id { get; set; }
+    public string name { get; set; }
+    [BsonElement("in_stock")]
+    public bool instock { get; set; }
+    [BsonRepresentation(BsonType.Double)]
+    public decimal price { get; set; }
+    public List<string> color { get; set; }
+}
+// end-model

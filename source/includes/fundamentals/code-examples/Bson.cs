7ca925efe4dcb44967055922c481bc53ed0a7f20
Mike Woofter <mike.woofter@mongodb.com>
2022-11-02 16:45:33 -0500

    wip

---
+++ source/includes/fundamentals/code-examples/Bson.cs
@@ -0,0 +1,31 @@
+using MongoDB.Driver;
+
+namespace Fundamentals;
+
+public class Bson
+{
+    public static void Main(string[] args)
+    {
+        //start-change
+        //start-create
+        var newRestaurant = new BsonDocument
+        {
+            { "address", new BsonDocument
+                {
+                    { "street", "Pizza St" },
+                    { "zipcode", "10003" }
+                }
+            },
+            { "coord", new BsonArray
+                {-73.982419, 41.579505 }
+            },
+            { "cuisine", "Pizza" },
+            { "name", "Mongo's Pizza" }
+        };
+        //end-create
+        newRestaurant.Add(new BsonElement("restaurant_id", "12345"));
+        newRestaurant.Remove("cuisine");
+        newRestaurant.Set("name", "Mongo's Pizza Palace");
+        //end-change
+    }
+}

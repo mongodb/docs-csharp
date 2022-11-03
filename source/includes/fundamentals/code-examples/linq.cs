af96343fec6f7edc3ecdf765d8b6e56467285f07
Jordan Smith <jordan.smith@mongodb.com>
2022-11-03 17:32:45 -0600

    initial linq page

---
+++ source/includes/fundamentals/code-examples/linq.cs
@@ -0,0 +1,35 @@
+
+// start-restaurant-model
+public class Restaurant
+{
+    public ObjectId Id { get; set; }
+
+    public string Name { get; set; }
+
+    [BsonElement("restaurant_id")]
+    public string RestaurantId { get; set; }
+
+    public string Cuisine { get; set; }
+
+    public object Address { get; set; }
+
+    public string Borough { get; set; }
+
+    public List<object> Grades { get; set; }
+}
+// end-restaurant-model
+
+// start-review-model
+
+public class Reviews
+{
+    public ObjectId Id { get; set; }
+
+    [BsonElement("restaurant_name")]
+    public string RestaurantName { get; set; }
+    public string Reviewer { get; set; }
+
+    public string ReviewText { get; set; }
+}
+
+// end-review-model

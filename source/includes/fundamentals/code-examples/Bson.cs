e96ab19d3d3ebac1d019b925771279109a5ecc2c
Mike Woofter <mike.woofter@mongodb.com>
2022-11-04 15:08:15 -0500

    fix

--- source/includes/fundamentals/code-examples/Bson.cs
+++ source/includes/fundamentals/code-examples/Bson.cs
@@ -6,7 +6,6 @@ public class Bson
 {
     public static void Main(string[] args)
     {
-        //start-change
         //start-create
         var newRestaurant = new BsonDocument
         {
@@ -23,6 +22,25 @@ public class Bson
             new BsonElement("name", "Mongo's Pizza")
         };
         //end-create
+    }
+
+    public static void ChangeDocument()
+    {
+        //start-change
+        var newRestaurant = new BsonDocument
+        {
+            { "address", new BsonDocument
+                {
+                    { "street", "Pizza St" },
+                    { "zipcode", "10003" }
+                }
+            },
+            { "coord", new BsonArray
+                {-73.982419, 41.579505 }
+            },
+            new BsonElement("cuisine", "Pizza"),
+            new BsonElement("name", "Mongo's Pizza")
+        };
         newRestaurant.Add(new BsonElement("restaurant_id", "12345"));
         newRestaurant.Remove("cuisine");
         newRestaurant.Set("name", "Mongo's Pizza Palace");

4f6b003c186671aa68c56ce98a15a3b929bcf442
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-02-24 14:19:10 -0500

    CC PR fixes

--- source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
+++ source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
@@ -1,4 +1,5 @@
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 // Replace the placeholders with your credentials
 const string connectionUri = "mongodb+srv://<username>:<password>@cluster0.sample.mongodb.net/?retryWrites=true&w=majority";
@@ -11,3 +12,7 @@ settings.ServerApi = serverApi;
 
 // Create a new client and connect to the server
 var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+Console.WriteLine(result.ToJson());

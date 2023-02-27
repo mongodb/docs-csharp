f0fb2f9a4b5569f293f71c14c9723452451ada40
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-02-27 10:50:56 -0500

    CC suggestions

--- source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
+++ source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
@@ -7,12 +7,13 @@ const string connectionUri = "mongodb+srv://<username>:<password>@cluster0.sampl
 var settings = MongoClientSettings.FromConnectionString(connectionString);
 
 // Set the ServerApi field of the settings object to Stable API version 1
-var serverApi = new ServerApi(ServerApiVersion.V1);
-settings.ServerApi = serverApi;
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
 
 // Create a new client and connect to the server
 var client = new MongoClient(settings);
 
 // Send a ping to confirm a successful connection
-var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-Console.WriteLine(result.ToJson());
+try {
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch(Exception e) { MongoTimeoutException exception;}

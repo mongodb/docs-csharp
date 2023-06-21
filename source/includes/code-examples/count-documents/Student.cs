098ad48529014adb74912b1e42cbb81d3de76b46
Nick LDP <nick.lopezdelpino@mongodb.com>
2023-06-21 11:18:27 -0400

    final

--- source/includes/code-examples/count-documents/Student.cs
+++ source/includes/code-examples/count-documents/Student.cs
@@ -1,7 +1,7 @@
 //start-grades-struct
-public class FinalGrades {
+public class Student {
     public ObjectId Id { get; set; }
-    public string StudentName { get; set; }
+    public string Name { get; set; }
     public double FinalGrade { get; set; }
 }
 //end-grades-struct

24f2b44b30e78e502cf0d6b17d8bc44ab5106e41
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2024-01-10 12:23:08 -0500

    Codewhisperper comments p5

--- source/includes/fundamentals/code-examples/specify-query/FindAndPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindAndPOCO.cs
@@ -1,6 +1,9 @@
+// Find all documents with am "establishedYear" value greater than 1985
+// and a "make" value of "Kiesel"
 var results = _guitarsCollection.Find(g => g.EstablishedYear >= 1985 && r.Make != "Kiesel").ToList();
 
 foreach (var doc in results)
 {
+    // Print the document in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }
--- source/includes/fundamentals/code-examples/specify-query/FindGtPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindGtPOCO.cs
@@ -1,6 +1,8 @@
+// Find all documents with am "establishedYear" value greater than 1985
 var results = _guitarsCollection.Find(g => g.EstablishedYear > 1985).ToList();
 
 foreach (var doc in results)
 {
+    // Print the document in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }

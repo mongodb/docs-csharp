c3abc4470c11798e4049843a5c0e9d140ff14557
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2024-01-10 15:48:00 -0500

    Codewhisperer comments p6

--- source/includes/fundamentals/code-examples/specify-query/FindGtBuilder.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindGtBuilder.cs
@@ -1,7 +1,11 @@
+// Creates a filter for all documents with an "establishedYear" greater than 1985
 var filter = Builders<Guitar>.Filter.Gt(g => g.EstablishedYear, 1985);
+
+// Find all documents that match the filter
 var result = _guitarsCollection.Find(filter).ToList();
 
 foreach (var doc in result)
 {
+    // Prints a document in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }

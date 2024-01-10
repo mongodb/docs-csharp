1fc2a1a886f6ca9c87a3e1ed39cc31498a2c772f
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2024-01-10 15:54:55 -0500

    pluralize

--- source/includes/fundamentals/code-examples/specify-query/FindEqPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindEqPOCO.cs
@@ -1,8 +1,8 @@
-// Find all documents with a "make" value of "Fender"
+// Finds all documents with a "make" value of "Fender"
 var results = _guitarsCollection.Find(g => g.Make == "Fender").ToList();
 
 foreach (var doc in results)
 {
-    // Print the document in bson (json) format
+    // Prints the document in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }
--- source/includes/fundamentals/code-examples/specify-query/FindGtPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindGtPOCO.cs
@@ -1,8 +1,8 @@
-// Find all documents with am "establishedYear" value greater than 1985
+// Finds all documents with am "establishedYear" value greater than 1985
 var results = _guitarsCollection.Find(g => g.EstablishedYear > 1985).ToList();
 
 foreach (var doc in results)
 {
-    // Print the document in bson (json) format
+    // Prints the document in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }

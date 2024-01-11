02f15655451808bd496688c03e0fea1fbc8ff6cd
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2024-01-11 10:30:16 -0500

    JS review

--- source/includes/fundamentals/code-examples/specify-query/FindAndPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindAndPOCO.cs
@@ -1,9 +1,9 @@
-// Finds all documents with am "establishedYear" value greater than 1985
-// and a "make" value of "Kiesel"
+// Finds all documents with an "establishedYear" value greater than 1985
+// and a "make" value that is not equal to "Kiesel"
 var results = _guitarsCollection.Find(g => g.EstablishedYear >= 1985 && r.Make != "Kiesel").ToList();
 
 foreach (var doc in results)
 {
-    // Prints the document in bson (json) format
+    // Prints the documents in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }
--- source/includes/fundamentals/code-examples/specify-query/FindEqPOCO.cs
+++ source/includes/fundamentals/code-examples/specify-query/FindEqPOCO.cs
@@ -3,6 +3,6 @@ var results = _guitarsCollection.Find(g => g.Make == "Fender").ToList();
 
 foreach (var doc in results)
 {
-    // Prints the document in bson (json) format
+    // Prints the documents in bson (json) format
     Console.WriteLine(doc.ToBsonDocument());
 }

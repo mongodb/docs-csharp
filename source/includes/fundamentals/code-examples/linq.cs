ab89fbfe36af6f2c14c993b1007d7cabfaf2a194
Maya Raman <maya.raman19@gmail.com>
2024-10-15 15:19:22 -0400

    never mind

--- source/includes/fundamentals/code-examples/linq.cs
+++ source/includes/fundamentals/code-examples/linq.cs
@@ -26,7 +26,7 @@ public class Address
     public string Building { get; set; }
 
     [BsonElement("coord")]
-    public double[] Coordinates { get; set; }
+    public float[] Coordinates { get; set; }
 
     public string Street { get; set; }
 

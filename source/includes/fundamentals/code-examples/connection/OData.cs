49e197fe9b28aa46479d6e9cfea6700a6379e537
Jordan Smith <jordan.smith@mongodb.com>
2024-11-05 12:36:23 -0800

    Add odata tutorial

---
+++ source/includes/fundamentals/code-examples/connection/OData.cs
@@ -0,0 +1,60 @@
+// start-controller
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using MongoDB.AspNetCore.OData;
+using MongoDB.Driver;
+using ODataTest.Models;
+
+namespace ODataTest.Controllers;
+
+public class RestaurantsController : ODataController
+{
+    private readonly IQueryable<Restaurant> _restaurants;
+
+    public RestaurantsController(IMongoClient client)
+    {
+        var database = client.GetDatabase("sample_restaurants");
+        _restaurants = database.GetCollection<Restaurant>("restaurants").AsQueryable();
+    }
+
+    // Register Get endpoint and set max documents to 5
+    [MongoEnableQuery(PageSize = 5)]
+    public ActionResult<IEnumerable<Restaurant>> Get()
+    {
+        var restaurantList = _restaurants.Take(5);
+        return Ok(restaurantList);
+    }
+}
+// end-controller
+
+// start-configure
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using ODataTest.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register a convention pack to convert fields to camel case
+var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+builder.Services.AddSingleton<IMongoClient>(new MongoClient("Your connection URI"));
+
+// Register the Restaurants entity and set the Id field as the key
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntitySet<Restaurant>("Restaurants");
+modelBuilder.EntityType<Restaurant>().HasKey(r => r.Id);
+
+// Add OData and specify query capabilities
+builder.Services.AddControllers().AddOData(
+    options => options.Select().AddRouteComponents("odata", modelBuilder.GetEdmModel())
+);
+
+var app = builder.Build();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
+// end-configure
